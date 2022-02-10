3b38802546dd6b6a31803ceafc9bf8aa3f1bac2b
mkizka <g.mkizka@gmail.com>
2022-02-10 14:42:42 +0900

    update

--- app/routes/packages/$name.tsx
+++ app/routes/packages/$name.tsx
@@ -4,6 +4,11 @@ import invariant from "tiny-invariant";
 export const loader: LoaderFunction = async ({ params }) => {
   invariant(params.name != undefined);
   const response = await fetch(`https://api.npms.io/v2/package/${params.name}`);
+  if (!response.ok) {
+    throw new Response("Not Found", {
+      status: 404,
+    });
+  }
   const data = await response.json();
   const url = data.collected.metadata.links.repository;
   return redirect(url, {

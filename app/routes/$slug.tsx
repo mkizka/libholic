58bd38c25eb3c5c699537b3487d1526a592e0567
mkizka <g.mkizka@gmail.com>
2022-02-10 15:22:46 +0900

    update

--- app/routes/$slug.tsx
+++ app/routes/$slug.tsx
@@ -1,4 +1,19 @@
-import { HeadersFunction, json, LoaderFunction, useLoaderData } from "remix";
+import {
+  Card,
+  Group,
+  Text,
+  Button,
+  useMantineTheme,
+  Badge,
+  SimpleGrid,
+} from "@mantine/core";
+import {
+  HeadersFunction,
+  json,
+  Link,
+  LoaderFunction,
+  useLoaderData,
+} from "remix";
 import invariant from "tiny-invariant";
 
 import { getDependenciesUser } from "~/utils/dependencies";
@@ -15,11 +30,11 @@ export const headers: HeadersFunction = () => {
 export const loader: LoaderFunction = async ({ params }) => {
   invariant(params.slug != undefined);
   const { rateLimit, dependencies } = await getDependenciesUser(params.slug);
-  const data = aggregate(dependencies)
+  const pkgs = aggregate(dependencies)
     .sort((a, b) => b.count - a.count)
     .slice(0, 30);
   return json(
-    { rateLimit, data },
+    { rateLimit, pkgs },
     {
       headers: cacheControl,
     }
@@ -27,10 +42,37 @@ export const loader: LoaderFunction = async ({ params }) => {
 };
 
 export default function Index() {
-  const data = useLoaderData();
+  const theme = useMantineTheme();
+  const { rateLimit, pkgs } = useLoaderData();
+  console.info(rateLimit);
   return (
-    <div>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-    </div>
+    <SimpleGrid
+      cols={4}
+      breakpoints={[
+        { maxWidth: "md", cols: 3 },
+        { maxWidth: "sm", cols: 2 },
+        { maxWidth: "xs", cols: 1 },
+      ]}
+    >
+      {pkgs.map((pkg: any) => (
+        <Card shadow="sm" padding="lg">
+          <Group>
+            <Badge color="green" variant="outline">
+              {pkg.count}
+            </Badge>
+            <Text weight={500}>{pkg.name}</Text>
+            <Button
+              component={Link}
+              to={`/packages/${pkg.name}`}
+              variant="light"
+              color="blue"
+              style={{ marginLeft: "auto" }}
+            >
+              Good First Issueを見てみる
+            </Button>
+          </Group>
+        </Card>
+      ))}
+    </SimpleGrid>
   );
 }
--- app/routes/packages/$name.tsx
+++ app/routes/packages/$name.tsx
@@ -4,17 +4,14 @@ import invariant from "tiny-invariant";
 export const loader: LoaderFunction = async ({ params }) => {
   invariant(params.name != undefined);
   const response = await fetch(`https://api.npms.io/v2/package/${params.name}`);
-  if (!response.ok) {
-    throw new Response("Not Found", {
-      status: 404,
-    });
+  if (response.ok) {
+    const data = await response.json();
+    const url = data?.collected?.metadata?.links?.repository;
+    if (url && typeof url == "string" && url.startsWith("https://github.com")) {
+      return redirect(`${url}/contribute`);
+    }
   }
-  const data = await response.json();
-  const url = data.collected.metadata.links.repository;
-  return redirect(url, {
-    headers: {
-      "Cache-Control":
-        "max-age=0, s-maxage=86400, stale-while-revalidate=86400",
-    },
+  throw new Response("Not Found", {
+    status: 404,
   });
 };
